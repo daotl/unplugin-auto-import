c3a5dc99e238e4a28be58d52de09b2ee4ab5665f
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-12-20 17:56:03 +0800

    feat: support nuxt@3.0.0

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,18 +1,11 @@
+import { addVitePlugin, addWebpackPlugin, defineNuxtModule } from '@nuxt/kit'
 import type { Options } from './types'
 import unplugin from '.'
 
-export default function (this: any, options: Options) {
-  options.exclude = options.exclude || [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/]
-
-  // install webpack plugin
-  this.extendBuild((config: any) => {
-    config.plugins = config.plugins || []
-    config.plugins.unshift(unplugin.webpack(options))
-  })
-
-  // install vite plugin
-  this.nuxt.hook('vite:extend', async (vite: any) => {
-    vite.config.plugins = vite.config.plugins || []
-    vite.config.plugins.push(unplugin.vite(options))
-  })
-}
+export default defineNuxtModule({
+  setup(options: Options) {
+    options.exclude = options.exclude || [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/]
+    addWebpackPlugin(unplugin.webpack(options))
+    addVitePlugin(unplugin.vite(options))
+  },
+})
